7bb07f044ed1f50efe9f02dfa61fe0dede8f98cb
Hollie-Mae Boittiaux <maeboittiaux@gmail.com>
2025-04-23 21:20:54 +0100

    feat(index.js): handle success or error events for opening database connection

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const request = window.indexedDB.open("todo-database", 1);
+let database;
+
+const request = indexedDB.open("todo-database", 1);
+
+request.onerror = () => {
+    console.error("[IndexedDB]: Error opening connection to database.");
+};
+request.onsuccess = (event) => {
+    console.log("[IndexedDB]: Database connection opened successfully.");
+    database = event.target.result;
+};
