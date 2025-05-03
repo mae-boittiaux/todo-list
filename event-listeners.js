5a268622f1fad06499a292d40a93b6545c136ed7
mae-boittiaux <maeboittiaux@gmail.com>
2025-05-03 20:54:06 +0100

    refactor(theme-buttons): move event-listener button logic to own file

---
+++ event-listeners.js
@@ -0,0 +1,61 @@
+import { updateTodoList } from "./index.js";
+
+export function setEventListeners() {
+    const themeButtons = document.querySelectorAll('.line-colour-button');
+
+    themeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            document.querySelector('.selected-theme')?.classList.remove('selected-theme');
+            button.classList.add('selected-theme');
+            updateTodoList();
+        })
+    });
+
+    document.getElementById('line-blue').onclick = () => {
+        localStorage.selectedTheme = 'line-blue';
+    };
+
+    document.getElementById('line-purple').onclick = () => {
+        localStorage.selectedTheme = 'line-purple';
+    };
+
+    document.getElementById('line-green').onclick = () => {
+        localStorage.selectedTheme = 'line-green';
+    };
+
+    document.querySelector('.selected-theme')?.classList.remove('selected-theme');
+    themeButtons.forEach(button => {
+        if (localStorage.selectedTheme == button.id) {
+            document.getElementById(button.id).classList.add('selected-theme');
+        }
+    });
+
+    const highlighterColour = document.querySelectorAll('.highlighter-colour-button');
+
+    highlighterColour.forEach(highlighter => {
+        highlighter.addEventListener('click', () => {
+            document.querySelector('.selected-highlighter')?.classList.remove('selected-highlighter');
+            highlighter.classList.add('selected-highlighter');
+            updateTodoList();
+        })
+    });
+
+    document.getElementById('highlighter-yellow').onclick = () => {
+        localStorage.selectedHighlighterColour = 'highlighter-yellow';
+    };
+
+    document.getElementById('highlighter-pink').onclick = () => {
+        localStorage.selectedHighlighterColour = 'highlighter-pink';
+    };
+
+    document.getElementById('highlighter-orange').onclick = () => {
+        localStorage.selectedHighlighterColour = 'highlighter-orange';
+    };
+
+    document.querySelector('.selected-highlighter')?.classList.remove('selected-highlighter');
+    highlighterColour.forEach(highlighter => {
+        if (localStorage.selectedHighlighterColour == highlighter.id) {
+            document.getElementById(highlighter.id).classList.add('selected-highlighter');
+        }
+    });
+}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { setEventListeners } from './event-listeners.js';
 import { logMessage, MessageScope } from './log-message.js';
 
 const databaseName = "todo-database";
@@ -8,6 +9,8 @@ let database;
 const request = indexedDB.open(databaseName, 1);
 
 const isTodoChecked = [];
+
+setEventListeners();
 
 request.onerror = () => {
     logMessage(MessageScope.INDEXEDDB, "Error opening connection to database");
@@ -44,7 +47,7 @@ document.getElementById('todo-form').onsubmit = (event) => {
     };
 };
 
-function updateTodoList() {
+export function updateTodoList() {
     const transaction = database.transaction([objectStoreName], 'readonly');
     const objectStore = transaction.objectStore(objectStoreName);
     const request = objectStore.getAll();
@@ -162,61 +165,3 @@ if ('serviceWorker' in navigator) {
             logMessage(MessageScope.SERVICE_WORKER, "Registration failed");
         })
 }
-
-const themeButtons = document.querySelectorAll('.line-colour-button');
-
-themeButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        document.querySelector('.selected-theme')?.classList.remove('selected-theme');
-        button.classList.add('selected-theme');
-        updateTodoList();
-    })
-});
-
-document.getElementById('line-blue').onclick = () => {
-    localStorage.selectedTheme = 'line-blue';
-};
-
-document.getElementById('line-purple').onclick = () => {
-    localStorage.selectedTheme = 'line-purple';
-};
-
-document.getElementById('line-green').onclick = () => {
-    localStorage.selectedTheme = 'line-green';
-};
-
-document.querySelector('.selected-theme')?.classList.remove('selected-theme');
-themeButtons.forEach(button => {
-    if (localStorage.selectedTheme == button.id) {
-        document.getElementById(button.id).classList.add('selected-theme');
-    }
-});
-
-const highlighterColour = document.querySelectorAll('.highlighter-colour-button');
-
-highlighterColour.forEach(highlighter => {
-    highlighter.addEventListener('click', () => {
-        document.querySelector('.selected-highlighter')?.classList.remove('selected-highlighter');
-        highlighter.classList.add('selected-highlighter');
-        updateTodoList();
-    })
-});
-
-document.getElementById('highlighter-yellow').onclick = () => {
-    localStorage.selectedHighlighterColour = 'highlighter-yellow';
-};
-
-document.getElementById('highlighter-pink').onclick = () => {
-    localStorage.selectedHighlighterColour = 'highlighter-pink';
-};
-
-document.getElementById('highlighter-orange').onclick = () => {
-    localStorage.selectedHighlighterColour = 'highlighter-orange';
-};
-
-document.querySelector('.selected-highlighter')?.classList.remove('selected-highlighter');
-highlighterColour.forEach(highlighter => {
-    if (localStorage.selectedHighlighterColour == highlighter.id) {
-        document.getElementById(highlighter.id).classList.add('selected-highlighter');
-    }
-});
